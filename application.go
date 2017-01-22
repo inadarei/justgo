397546a2d758db54ab423c1b3458f567279892bd
Irakli N <inadarei@users.noreply.github.com>
2017-01-22 01:07:17 -0500

    init

---
+++ application.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	serverPort := os.Getenv("PORT")
+	if serverPort == "" {
+		serverPort = "3737"
+		log.Print("WARNING: no server port supplied. Defaulting to " + serverPort)
+	}
+
+	http.HandleFunc("/", viewHandler)
+	err := http.ListenAndServe(":"+serverPort, nil)
+	if err != nil {
+		log.Fatal("ERROR: couldn't start server: ", err)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/plain")
+
+	fmt.Fprintf(w, "Hello world! This is great.")
+}
