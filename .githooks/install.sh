53cf1ad2c25d9e6ef5bac2c5648197de0c5c40eb
Irakli N <inadarei@users.noreply.github.com>
2017-12-15 18:13:18 -0500

    permanent githooks polished

--- .githooks/install.sh
+++ .githooks/install.sh
@@ -2,5 +2,8 @@
 
 # link pre-commit from .git/hooks to here so every checkout has it
 gitpath=`git rev-parse --git-dir`
-projectpath=`dirname "$gitpath"`
-ln -s -f "$projectpath/.githooks/pre-commit" "$gitpath/hooks/pre-commit"
+projectpath="$(dirname $gitpath)/"
+if test "$projectpath" = './'; then
+  projectpath=$(pwd)
+fi
+ln -s -f "$projectpath.githooks/pre-commit" "$gitpath/hooks/pre-commit"
