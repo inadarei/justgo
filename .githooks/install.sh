e2c92c621b216f899fd6cfa22c901fb3c6041256
Irakli N <inadarei@users.noreply.github.com>
2017-12-15 17:39:56 -0500

    adding persistent githooks

---
+++ .githooks/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# link pre-commit from .git/hooks to here so every checkout has it
+gitpath=`git rev-parse --git-dir`
+projectpath=`dirname "$gitpath"`
+ln -s -f $projectpath/pre-commit $gitpath/hooks/pre-commit
